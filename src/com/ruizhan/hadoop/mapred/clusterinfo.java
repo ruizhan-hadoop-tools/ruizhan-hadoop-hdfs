49471d87b3f1a582980aec7f0ef63976a3895ca9
ruizhan-qianchao <qianchao@game-reign.com>
2014-11-25 18:20:58 +0800

    change clusterinfo

--- src/com/ruizhan/hadoop/mapred/clusterinfo.java
+++ src/com/ruizhan/hadoop/mapred/clusterinfo.java
@@ -1,6 +1,8 @@
 package com.ruizhan.hadoop.mapred;
 
 import java.net.InetSocketAddress;
+import java.util.Collection;
+import java.util.Iterator;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapred.ClusterStatus;
@@ -11,12 +13,24 @@ public class clusterinfo  {
 
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
-		JobClient cli = new JobClient(new InetSocketAddress("hadoop-808-01.hzfh",8021),conf);
-		ClusterStatus cluster_status_1 = cli.getClusterStatus();
-		System.err.println(cluster_status_1.getMaxMemory());
-		System.err.println(cluster_status_1.getMapTasks());
-		System.err.println(cluster_status_1.getMaxMapTasks());
+		JobClient cli = new JobClient(new InetSocketAddress("10.6.252.77",8021),conf);
+		ClusterStatus cluster_status = cli.getClusterStatus();
+		System.out.println("MaxMapTasks:" + new Integer(cluster_status.getMaxMapTasks()).toString());
+		System.out.println("MapTasks:" + new Integer(cluster_status.getMapTasks()).toString());
+		System.out.println("MaxReduceTasks:" + new Integer(cluster_status.getMaxReduceTasks()).toString());
+		System.out.println("ReduceTasks:" + new Integer(cluster_status.getReduceTasks()).toString());
+		System.out.println("BlacklistedTrackersNums:" + new Integer(cluster_status.getBlacklistedTrackers()).toString());
+		System.out.println("BlacklistedTrackers:" + transFromCollection(cluster_status.getBlacklistedTrackerNames()));
 	
+	}
+	
+	private static String transFromCollection(Collection<?> collect){
+		String line = new String("");
+		Iterator itr = collect.iterator();
+		while (itr.hasNext()) {
+			line.concat(itr.next().toString()+";");
+		}
+		return line;
 	}
 
 
