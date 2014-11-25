b114fb20366fa73ba641786987dda19cda0c4e6d
ruizhan-qianchao <qianchao@game-reign.com>
2014-11-25 17:44:46 +0800

    change clusterinfo

--- src/com/ruizhan/hadoop/mapred/clusterinfo.java
+++ src/com/ruizhan/hadoop/mapred/clusterinfo.java
@@ -1,50 +1,22 @@
 package com.ruizhan.hadoop.mapred;
 
 import java.net.InetSocketAddress;
-import java.net.URL;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.mapred.ClusterStatus;
 import org.apache.hadoop.mapred.JobClient;
-import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.mapreduce.Cluster;
-import org.apache.hadoop.mapreduce.ClusterMetrics;
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
 
-public class clusterinfo extends Configured implements Tool {
 
-	public int run(String[] args) throws Exception {
-		JobConf jobConf = new JobConf(getConf());
-		JobClient client = new JobClient(jobConf);
-	    ClusterStatus cluster = client.getClusterStatus();
-	    System.err.println(cluster.getActiveTrackerNames());
-		return 0;
+public class clusterinfo  {
 
-	}
-	
 	public static void main(String[] args) throws Exception {
-	    //int res = ToolRunner.run(new Configuration(), new Sort(), args);
-	    //System.exit(res);
-		String className = JobClient.class.getName();  
-		className = className.replace('.', '/');  
-		String resource = "/" + className + ".class";  
-		URL url = JobClient.class.getResource(resource);  
-		System.out.println(url.getFile());
 		Configuration conf = new Configuration();
-		JobClient cli = new JobClient(new InetSocketAddress("10.6.252.77",8021),conf);
-		//Cluster cluster = new Cluster(new InetSocketAddress("10.6.252.77",8021),conf);
-		//ClusterMetrics cluster_status_0 = cluster.getClusterStatus();
+		JobClient cli = new JobClient(new InetSocketAddress("hadoop-808-01.hzfh",8021),conf);
 		ClusterStatus cluster_status_1 = cli.getClusterStatus();
-		//System.err.println(cluster_status_0.getMapSlotCapacity());
-		//System.err.println(cluster_status_0.getReservedMapSlots());
 		System.err.println(cluster_status_1.getMaxMemory());
 		System.err.println(cluster_status_1.getMapTasks());
-		//Cluster cluster = cli.getClusterHandle();
-		//ClusterMetrics cluster_status_0 = cluster.getClusterStatus();
-		//System.err.println(cluster_status_0.getMapSlotCapacity());
-		//System.err.println(cluster_status_0.getReservedMapSlots());		
+		System.err.println(cluster_status_1.getMaxMapTasks());
+	
 	}
 
 
